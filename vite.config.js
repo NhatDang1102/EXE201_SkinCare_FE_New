e4e5a61ecadb11b93b68185b6b888cc1ca68a665
Vukobitcode <vundlse171424@fpt.edu.vn>
2025-06-14 21:25:55 +0700

    Add Tailwind CSS support and update dependencies
    - Added Tailwind CSS and its Vite plugin to package.json.
    - Imported Tailwind CSS in App.css.
    - Updated box-shadow styles for Toastify notifications for consistency.
    - Configured Vite to use Tailwind CSS plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import mkcert from 'vite-plugin-mkcert';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import mkcert from "vite-plugin-mkcert";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     mkcert(), // Enable mkcert for local HTTPS
+    tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@mui/styled-engine': '@mui/styled-engine-sc'
+      "@mui/styled-engine": "@mui/styled-engine-sc",
     },
   },
   server: {
